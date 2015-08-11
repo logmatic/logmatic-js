20d1d6c685b4a035c39f2ccbcfde25fce2430481
Renaud Boutet <rb@focusmatic.com>
2015-08-11 11:02:04 +0200

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options : {
+                sourceMap: true
+            },
+            main: {
+                files: { 'src/logmatic.min.js': ['src/logmatic.js'] }
+            }
+        }
+	});
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default', ['uglify'] );
+};
---
+++ src/logmatic.js
@@ -0,0 +1,156 @@
+
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.logmatic = factory();
+  }
+}(this, function () {
+  var _url;
+  var _ipTrackingAttr;
+  var _uaTrackingAttr;
+
+  var init = function(key) {
+    _url = "https://api.logmatic.io/v1/input/"+key;
+  }
+
+  var log = function(message, properties) {
+    if (!_url) {
+      console.error("Please init Logmatic before pushing events"); return;
+    }
+    var payload = {message: message};
+    if (_metas) {
+      for (var key in _metas) {
+        if (_metas.hasOwnProperty(key)) {
+          payload[key] = _metas[key];
+        }
+      }
+    }
+    //Properties can override metas
+    if (properties) {
+      for (var key in properties) {
+        if (properties.hasOwnProperty(key)) {
+          payload[key] = properties[key];
+        }
+      }
+    }
+    post(payload);
+  }
+
+  var post = function(data, successFn, errorFn) {
+    var request = new XMLHttpRequest();
+    request.open('POST', _url, true);
+    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+    //Enable IP tracking
+    if(_ipTrackingAttr){
+      request.setRequestHeader('X-Logmatic-Add-IP', _ipTrackingAttr);
+    }
+    //Enable UserAgent tracking
+    if(_uaTrackingAttr){
+      request.setRequestHeader('X-Logmatic-Add-UserAgent', _uaTrackingAttr);
+    }
+
+    request.onload = function() {
+      if (successFn) successFn(request);
+    };
+
+    request.onerror = function() {
+      if (errorFn) errorFn(request);
+    };
+
+    request.send(JSON.stringify(data));
+  }
+
+  var setMetas = function(metas) {
+    _metas = metas;
+  }
+
+  function setSendConsoleLogs(consoleLevelAttribute) {
+    var oldLog = console.log;
+    console.log = function (message) {
+        var props = {};
+        if(consoleLevelAttribute){
+          props[consoleLevelAttribute]="info";
+        }
+        log(message,props);
+        oldLog.apply(console, arguments);
+    };
+    var oldInfo = console.info;
+    console.info = function (message) {
+        var props = {};
+        if(consoleLevelAttribute){
+          props[consoleLevelAttribute]="log";
+        }
+        log(message,props);
+        oldInfo.apply(console, arguments);
+    };
+    var oldTrace = console.trace;
+    console.trace = function (message) {
+        var props = {};
+        if(consoleLevelAttribute){
+          props[consoleLevelAttribute]="trace";
+        }
+        log(message,props);
+        oldTrace.apply(console, arguments);
+    };
+    var oldWarn = console.warn;
+    console.warn = function (message) {
+        var props = {};
+        if(consoleLevelAttribute){
+          props[consoleLevelAttribute]="warn";
+        }
+        log(message,props);
+        oldWarn.apply(console, arguments);
+    };
+    var oldError = console.error;
+    console.error = function (message) {
+        var props = {};
+        if(consoleLevelAttribute){
+          props[consoleLevelAttribute]="error";
+        }
+        log(message,props);
+        oldError.apply(console, arguments);
+    };
+  }
+
+  function setSendConsoleErrors(errorAttribute) {
+    if(errorAttribute){
+      var oldhandler = window.onerror;
+      window.onerror = function (message, url, line, col){
+        var errorProperties = {};
+        errorProperties[errorAttribute] = {
+          type: "JSException",
+          url: url,
+          line: line,
+          col: col,
+        };
+        log(message,errorProperties);
+
+        if (oldhandler && typeof oldhandler === 'function') {
+          oldhandler.apply(window, arguments);
+        }
+      };
+    }
+  }
+
+  var setIPTracking = function(ipTrackingAttr) {
+    _ipTrackingAttr = ipTrackingAttr;
+  }
+
+  var setUserAgentTracking = function(uaTrackingAttr) {
+    _uaTrackingAttr = uaTrackingAttr;
+  }
+
+  return {
+    init: init,
+    log: log,
+    setMetas: setMetas,
+    setSendConsoleErrors: setSendConsoleErrors,
+    setSendConsoleLogs: setSendConsoleLogs,
+    setIPTracking: setIPTracking,
+    setUserAgentTracking: setUserAgentTracking
+  };
+}));
