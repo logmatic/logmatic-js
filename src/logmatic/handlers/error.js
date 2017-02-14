595d55293ff622d5b8f6d1ae9180410c398ed4a9
Guillaume Polaert <guillaume@logmatic.io>
2017-02-14 15:35:10 +0100

    add tracekit deps

--- src/logmatic/handlers/error.js
+++ src/logmatic/handlers/error.js
@@ -3,22 +3,39 @@ module.exports = function (logger, props) {
   var _errorAttributeName = props.errorAttributeName || "error";
   var _levelAttributeName = props.levelAttributeName || "level";
 
-  var oldHandler = window.onerror;
-  window.onerror = function (message, url, line, column) {
-    var errorProperties = {};
-    errorProperties[_errorAttributeName] = {
-      mode: 'JSException',
-      url: url,
-      line: line,
-      column: column,
-      name: message
-    };
 
-    errorProperties[_levelAttributeName] = "error";
-    logger.log(message, errorProperties);
-    if (oldHandler && (typeof(oldHandler) === 'function')) {
-      oldHandler.apply(window, arguments);
-    }
-  };
+  //Use TraceKit if available, fallback on basic error reporting otherwise
+  if (window && window.TraceKit) {
+    var TraceKit = window.TraceKit;
+    TraceKit.report.subscribe(function (errorReport) {
+      var errorProperties = {};
+      errorProperties[_errorAttributeName] = errorReport;
+      errorProperties[_levelAttr] = "error";
+      errorProperties[_errorAttributeName].name = errorReport.message;
+      errorProperties[_errorAttributeName].type = errorReport.message.replace(/:.*/, "");
+      log(errorReport.message, errorProperties);
+    });
+  } else {
+
+    var oldHandler = window.onerror;
+    window.onerror = function (message, url, line, column) {
+      var errorProperties = {};
+      errorProperties[_errorAttributeName] = {
+        mode: 'JSException',
+        file: url.replace(window.location.origin, ""),
+        type: message.replace(/:.*/, ""),
+        url: url,
+        line: line,
+        column: column,
+        name: message
+      };
+
+      errorProperties[_levelAttributeName] = "error";
+      logger.log(message, errorProperties);
+      if (oldHandler && (typeof(oldHandler) === 'function')) {
+        oldHandler.apply(window, arguments);
+      }
+    };
+  }
 };
 
